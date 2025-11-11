fc540559c2a091744eb58947faa0b87493ca624b
mike <h@solfunmeme.com>
2025-11-11 21:37:06 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -3,24 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
-    #cargo2nix.url = "path:/data/data/com.termux.nix/files/home/pick-up-nix2/vendor/nix/cargo2nix";
-    cargo2nix.url = "github:meta-introspector/cargo2nix?ref=feature/CRQ-016-nixify";
+    cargo2nix.url = "path:/data/data/com.termux.nix/files/home/pick-up-nix2/vendor/nix/cargo2nix";
     flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
-#    time-macros.url = "github:meta-introspector/time-rs?ref=feature/CRQ-016-nixify";
+#    time-macros = { url = "github:meta-introspector/time-rs?ref=feature/CRQ-016-nixify"; };
   };
 
   outputs = { self, nixpkgs, cargo2nix, flake-utils }: 
     let
-      timeRsOverlay = import ./overlay.nix;
       pkgs = import nixpkgs {
         system = "aarch64-linux";
-        overlays = [ cargo2nix.overlays.default timeRsOverlay ];
+        overlays = [ cargo2nix.overlays.default ];
       };
       rustPkgs = pkgs.rustBuilder.makePackageSet {
         packageFun = import ./Cargo.nix;
         rustChannel = "stable";
         rustVersion = "1.81.0";
         rootFeatures = [ "time-macros/large-dates" ];
+        packageOverrides = pkgs: [ # Add this block
+          (pkgs.rustBuilder.rustLib.makeOverride {
+            name = "time-macros";
+            overrideAttrs = old: {
+              rustcBuildFlags = (old.rustcBuildFlags or []) ++ [ "--allow=unknown_lints" ];
+            };
+          })
+        ];
       };
     in
     {
