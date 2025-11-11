ae0317846278278c241dd5117811b4dd7c0d2bcf
mike <h@solfunmeme.com>
2025-11-11 19:40:45 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
     cargo2nix.url = "github:meta-introspector/cargo2nix?ref=feature/CRQ-016-nixify"; # Assuming this is the correct cargo2nix flake
     flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify"; # Add flake-utils input
+    time-macros = { url = "github:time-rs/time?dir=time-macros" };
   };
 
   outputs = { self, nixpkgs, cargo2nix, flake-utils }: 
@@ -18,6 +19,7 @@
         packageFun = import ./Cargo.nix;
         rustChannel = "stable";
         rustVersion = "1.81.0";
+        rootFeatures = [ "time-macros/large-dates" ];
       };
     in
     {
